43856151b9a6bcc332471fdbe4b32e952962d33d
Omar Cardoso <omarmello18@gmail.com>
2022-02-16 17:58:46 -0300

    Incluido função e tela de autenticação

--- agenda/urls.py
+++ agenda/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core2 import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 #    path('eventos/<titulo_evento>/', views.eventos),
     path('agenda/', views.lista_eventos),
-    path('', views.index)
+    path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
+    path('agenda/evento/', views.evento),
+    path('agenda/evento/submit', views.submit_evento)
+
 ]
--- core2/views.py
+++ core2/views.py
@@ -1,15 +1,61 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render
+from django.shortcuts import redirect
 from core2.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 # Create your views here.
 
 #def eventos(request, titulo_evento):
 #    Evento.objects.get(titulo = titulo_evento)
 #    return HttpResponse(Evento.local)
 
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "usuário ou senha invalido")
+    else:
+        redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    evento = Evento.objects.all()
+    user = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
-def index(request):
-    return redirect('/agenda/')
+#def index(request):
+#    return redirect('/agenda/')
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        local = request.POST.get('local')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              local=local,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
